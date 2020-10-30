e32946e1853937a38cf1842b3ceb4a4a2e0caf15
asha-keta <asha.b114@gmail.com>
2020-10-30 15:35:40 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -18,21 +18,11 @@ pipelineJob('Ansible_DSL_Demo1') {
     parameters {
         stringParam('Playbook_Name', 'linux_server_discovery', 'Playbook to trigger')
         stringParam('Playbook_Name1', 'linux_server_discovery1', 'Playbook to trigger1')
-    }
-     
+    }     
     definition {
         cpsScm {
           scm {
             git(repo, 'master', { node -> node / 'extensions' << '' } )
-              configure { git ->
-                git / 'extensions' / 'hudson.plugins.git.extensions.impl.SparseCheckoutPaths' / 'sparseCheckoutPaths' {
-                    ['linux_server_discovery'].each { mypath ->
-                        'hudson.plugins.git.extensions.impl.SparseCheckoutPath' {
-                            path("${mypath}")
-                }
-            }
-        }
-	}
             }
             scriptPath('linux_server_discovery/Pipeline/Jenkinsfile')
         }
@@ -44,8 +34,7 @@ job("free-style-ansible") {
     parameters {
         stringParam('Playbook_Name', 'linux_server_discovery', 'Playbook to trigger')
         stringParam('Playbook_Name1', 'linux_server_discovery1', 'Playbook to trigger1')
-    }
-    
+    }    
     steps {
         shell "docker exec trusting_lovelace sh -c 'ansible-playbook /ansible_playbooks/workspace/Sonar_Scanner/linux_server_discovery/Playbooks/linux_discovery.yml'"
     }
